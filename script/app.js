d713d8a7c4116f487230ec52c1e46f339d1b91fb
sleepykanguru22 <70289696+sleepykanguru22@users.noreply.github.com>
2022-10-18 09:15:19 -0700

    notes

---
+++ script/app.js
@@ -0,0 +1,7 @@
+/*Create an "ask" function that when clicked, will choose a random 8ball image and insert it into your "answers" div */
+
+const ask = () => {
+  //return answer as:
+  //return a random picture
+  //return user input
+};
